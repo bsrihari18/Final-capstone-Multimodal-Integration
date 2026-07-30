ad3221af4b176487a56c36a49f673f14ea98be82
bsrihari18 <bsrihari0918@gmail.com>
2026-07-30 17:01:02 +0530

    Update webcam.py

--- utils/webcam.py
+++ utils/webcam.py
@@ -1,18 +1,32 @@
-from streamlit_webrtc import VideoProcessorBase
-import av
+import cv2
+import numpy as np
+from PIL import Image
 
 from utils.face_detection import process_frame
 
 
-class EmotionProcessor(VideoProcessorBase):
+class EmotionProcessor:
+    """
+    Processes images captured from Streamlit camera_input().
+    """
 
-    def recv(self, frame):
+    @staticmethod
+    def process(uploaded_image):
+        """
+        uploaded_image : UploadedFile returned by st.camera_input()
+        """
 
-        image = frame.to_ndarray(format="bgr24")
+        if uploaded_image is None:
+            return None
 
+        # Convert uploaded image to numpy array
+        image = Image.open(uploaded_image).convert("RGB")
+        image = np.array(image)
+
+        # RGB -> BGR (OpenCV format)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Detect face + predict emotion
         image = process_frame(image)
 
-        return av.VideoFrame.from_ndarray(
-            image,
-            format="bgr24"
-        )
+        return image
